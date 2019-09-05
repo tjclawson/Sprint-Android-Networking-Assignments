0d1385043fb4e469e6b04ec3499a894ae33c01f5
blcooley <663897+blcooley@users.noreply.github.com>
2019-09-05 10:28:14 -0500

    Remove unneeded call from NetworkAdapter in M04 Guided project

--- M04/GuidedProject/app/src/main/java/com/lambdaschool/serviceimagedownloader/NetworkAdapter.kt
+++ M04/GuidedProject/app/src/main/java/com/lambdaschool/serviceimagedownloader/NetworkAdapter.kt
@@ -19,62 +19,6 @@ object NetworkAdapter {
     val DELETE = "DELETE"
     val TIMEOUT = 3000
 
-    // only handles get and post requests right now
-    @JvmOverloads
-    fun httpRequest(stringUrl: String, requestType: String, body: String = ""): String {
-        var result = ""
-        var stream: InputStream? = null
-        var connection: HttpURLConnection? = null
-        try {
-            val url = URL(stringUrl)
-            connection = url.openConnection() as HttpURLConnection
-            connection.readTimeout = TIMEOUT
-            connection.connectTimeout = TIMEOUT
-            connection.requestMethod = requestType
-
-            if (requestType == GET || requestType == DELETE) {
-                connection.connect()
-            } else if (requestType == POST || requestType == PUT) {
-                val outputStream = connection.outputStream
-                outputStream.write(body.toByteArray())
-                outputStream.close()
-            }
-
-            if (connection.responseCode == HttpURLConnection.HTTP_OK) {
-                stream = connection.inputStream
-                if (stream != null) {
-                    val reader = BufferedReader(InputStreamReader(stream))
-                    val builder = StringBuilder()
-                    var line = reader.readLine()
-                    while (line != null) {
-                        builder.append(line)
-                        line = reader.readLine()
-                    }
-                    result = builder.toString()
-                }
-            }
-
-        } catch (e: MalformedURLException) {
-            e.printStackTrace()
-            result = e.message ?: ""
-        } catch (e: IOException) {
-            e.printStackTrace()
-            result = e.message ?: ""
-        } finally {
-            connection?.disconnect()
-
-            if (stream != null) {
-                try {
-                    stream.close()
-                } catch (e: IOException) {
-                    e.printStackTrace()
-                }
-
-            }
-        }
-        return result
-    }
-
     fun getBitmapFromUrl(stringUrl: String): Bitmap? {
         var result: Bitmap? = null
         var stream: InputStream? = null
